16d8b05091e805f6182f69b5d719346350edb8be
FurryFur <lance.e.chaney@gmail.com>
2019-08-16 13:04:02 +1200

    Init

---
+++ ValCacheServerHost/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ValCacheServerLib;
+
+namespace ValCacheServerHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(CalculatorService)))
+            {
+                // Start the service.
+                host.Open();
+                Console.WriteLine("The service is ready.");
+
+                Console.WriteLine("Press <Enter> to terminate the service.");
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+        }
+    }
+}
